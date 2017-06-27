63415bcf6323a24d2e72a09c5907305c8d5ad5a6
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2017-06-27 12:06:30 -0400

    ?

--- modem.go
+++ modem.go
@@ -70,6 +70,11 @@ func Modem_Init() {
 	Modem_Reader.ReadBytes('\n')
 	Modem_Reader.ReadBytes('\n')
 	Modem_Reader.ReadBytes('\n')
+	Modem_Reader.ReadBytes('\n')
+	Modem_Reader.ReadBytes('\n')
+	Modem_Reader.ReadBytes('\n')
+	Modem_Reader.ReadBytes('\n')
+	Modem_Reader.ReadBytes('\n')
 	Modem_Discard()
 
 	if Modem_GetReply("AT") != "OK" {
